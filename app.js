84649e3cf580e08766916cd61985109b591684c2
mumbling-brick <123447936+mumbling-brick@users.noreply.github.com>
2025-10-04 21:26:41 +0900

    Recap

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);
-    paintGreeting(username);
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    paintGreeting();
     loginForm.classList.add(HIDDEN_CLASSNAME);
 }
 
-function paintGreeting(username) {
+function paintGreeting() {
+    const username = localStorage.getItem(USERNAME_KEY);
     greeting.classList.remove(HIDDEN_CLASSNAME);
     greeting.innerText = `Hello ${username}`;
 }
@@ -29,5 +29,5 @@ if(savedUsername === null) {
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
     //show greeting
-    paintGreeting(savedUsername);
+    paintGreeting();
 }
