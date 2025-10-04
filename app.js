1a7208e85fab5c301be987bd637c162bfa73de34
mumbling-brick <123447936+mumbling-brick@users.noreply.github.com>
2025-10-04 21:06:09 +0900

    Loading Username

--- app.js
+++ app.js
@@ -3,16 +3,31 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const link = document.querySelector("a");
 
+
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
-    localStorage.setItem("username", username);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreeting(username);
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    console.log(username);
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreeting(username) {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+}
+
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    //show greeting
+    paintGreeting(savedUsername);
+}
