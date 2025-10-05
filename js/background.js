b5668f28950be1411bf6b2cf76ee77e21cb534ae
mumbling-brick <123447936+mumbling-brick@users.noreply.github.com>
2025-10-05 13:11:58 +0900

    Background

---
+++ js/background.js
@@ -0,0 +1,7 @@
+const images = ["0.jpeg", "1.jpeg", "2.jpeg",];
+
+const chosenImg = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+bgImage.src = `img/${chosenImg}`;
+document.body.appendChild(bgImage);
